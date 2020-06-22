df722a77260a57286bb75039e813f3a5271e758c
Mickael Araujo <araujomickael18@gmail.com>
2020-06-22 06:13:50 -0300

    :lipstick: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'
+
+import { FooterContainer } from './styles'
 
 export default function Footer() {
   return (
-    // <h1>Footer</h1>
-    <div></div>
+    <FooterContainer>
+      <div>
+        <a href='https://github.com/MickaelAraujs'>
+          <FaGithub size={45} color='#429586' />
+        </a>
+
+        <a href='https://www.linkedin.com/in/mickaelaraujs/'>
+          <FaLinkedin size={45} color='#429586' />
+        </a>
+
+        <a href='https://www.instagram.com/mickaelarauj/'>
+          <FaInstagram size={45} color='#429586' />
+        </a>
+      </div>
+    </FooterContainer>
   )
 }
