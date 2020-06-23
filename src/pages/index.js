e34ae011e67626e8fab92fc724be754802849bd0
Mickael Araujo <araujomickael18@gmail.com>
2020-06-23 02:43:38 -0300

    :sparkles: about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,65 @@
 import React from 'react'
+import {
+  FaHtml5, FaCss3, FaJsSquare,
+  FaNodeJs, FaGithub, FaReact
+} from 'react-icons/fa'
 
 import { Container } from '../styles/global'
+import { AboutContainer, Profile, Description } from '../styles/about'
 
 import Layout from '../components/Layout'
+
+import profile from '../assets/me.jpeg'
 
 export default function Home() {
   return (
     <Layout>
       <Container>
-        <h1>Hello World!</h1>
+        <AboutContainer>
+          <Profile>
+            <img src={profile} alt='Mickael Araujo' />
+
+            <div className="techs">
+              <FaHtml5 />
+
+              <FaCss3 />
+
+              <FaJsSquare />
+
+              <FaGithub />
+
+              <FaNodeJs />
+
+              <FaReact />
+            </div>
+          </Profile>
+
+          <Description>
+            <h1>
+              Mickael Araujo, <span>Full-stack Developer</span>
+            </h1>
+
+            <p>
+              Estudante de <span>Análise e Desenvolvimento de Sistemas</span>,
+              apaixonado por tecnologias e disposto a sempre aprender coisas novas;
+              principalmente tecnologias relacionadas ao Javascript.
+              Aspirante a programador desde abril/2019. Desde então, busco evoluir mais a cada dia.  
+            </p>
+
+            <h2>know-how:</h2>
+
+            <ul>
+              <li>Desenvolvimento de APIs REST com NodeJS e Express</li>
+              <li>Desenvolvimento de Single Page Applications com ReactJS</li>
+              <li>Desenvolvimento de Aplicações mobile cross-platform com React Native</li>
+              <li>HTML5 e CSS3</li>
+              <li>Typescript</li>
+              <li>Responsividade de páginas web</li>
+              <li>Estilizando aplicações React com styled components</li>
+              <li>Git</li>
+            </ul>
+          </Description>
+        </AboutContainer>
       </Container>
     </Layout>
   )
---
+++ src/styles/about.js
@@ -0,0 +1,95 @@
+import styled, { keyframes } from 'styled-components'
+
+const Slide = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const AboutContainer = styled.div`
+  display: flex;
+  animation: ${Slide} 0.8s forwards;
+`
+
+export const Profile = styled.div`
+  width: 50vw;
+  height: 70vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 195px;
+    border-radius: 50%;
+    margin-top: 16px;
+  }
+
+  .techs {
+    margin-top: 48px;
+    width: 250px;
+    display: grid;
+    grid-template-columns: 48px 48px 48px;
+    justify-content: center;
+    align-items: center;
+
+    svg {
+      width: 26px;
+      height: 26px;
+      color: #429586;
+      margin: 8px;
+    }
+  }
+`
+
+export const Description = styled.div`
+  padding-top: 16px;
+  padding-right: 54px;
+  h1 {
+    color: #429586;
+    font-size: 48px;
+    margin-left: 8px;
+
+    span {
+      font-size: 24px;
+      font-weight: 400;
+      font-style: italic;
+    }
+  }
+
+  p {
+    margin-top: 8px;
+    margin-left: 8px;
+    font-size: 18px;
+    color: #9C9999;
+
+    span {
+      color: #429586;
+      font-weight: bold;
+    }
+  }
+
+  h2 {
+    font-size: 24px;
+    color: #429586;
+    margin-top: 24px;
+    margin-left: 8px;
+  }
+
+  ul {
+    list-style: circle;
+    list-style-position: inside;
+    margin-left: 8px;
+    margin-top: 8px;
+
+    li {
+      font-size: 14px;
+      color: #9C9999;
+      margin-top: 8px;
+    }
+  }
+`
